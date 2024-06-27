66afe5d13cfafa285f390f985a2b82ab2a09af80
Tam <115720528+NguyenTam03@users.noreply.github.com>
2024-06-27 21:37:30 +0700

    Create link_url

---
+++ link_url.py
@@ -0,0 +1,18 @@
+#1 
+url_1 = [
+    "https://www.facebook.com/xxx",
+    "https://www.facebook.com/xxx",
+]   
+#2
+url_2=[
+    "https://www.facebook.com/xxx",
+    "https://www.facebook.com/xxx",
+]
+#3
+url_3=[
+    "https://www.facebook.com/xxx",
+    "https://www.facebook.com/xxx",
+]
+
+
+
